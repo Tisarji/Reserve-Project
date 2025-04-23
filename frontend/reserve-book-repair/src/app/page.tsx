8ba94ee49dcea16b22095ef250d8fcf23bf480fe
Tisarji <jirasitkarunwong@gmail.com>
2025-04-24 00:05:13 +0700

    error line children

--- frontend/reserve-book-repair/src/app/page.tsx
+++ frontend/reserve-book-repair/src/app/page.tsx
@@ -1,23 +1,8 @@
-import RoomManagement from "@/component/management/room";
-import UserManagement from "@/component/management/user";
-
-import Login from "../component/login/login";
-import Navbar from "../component/navbar/navbar";
-import NotificationPanel from "@/component/notification/notification";
 //import { User } from "lucide-react";
 
 export default function Home() {
 	return (
 		<>
-			{/*<Login />*/}
-			<main className="p-6">
-				{/* <RoomManagement /> */}
-        
-				{/* <UserManagement /> */}
-
-        {/* <History /> */}
-
-			</main>
 		</>
 	);
 }
