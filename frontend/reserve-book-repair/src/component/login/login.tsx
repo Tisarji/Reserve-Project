572745edd45c8d561771156d7fb197457226697e
PMOON5681 <ph.thanadon.13@gmail.com>
2025-04-23 20:28:28 +0700

    petch:login

--- frontend/reserve-book-repair/src/component/login/login.tsx
+++ frontend/reserve-book-repair/src/component/login/login.tsx
@@ -1,7 +1,50 @@
+'use client'
+
+import React, { useState } from "react";
+
+const handleSubmit = (e:any) => {
+	e.preventDefault();
+};
+
+const forgotpassword = () => {
+};
 
 function Login() {
+	const [inputUsername, setInputUsername] = useState("");
+	const [inputPassword, setInputPassword] = useState("");
 	return (
-		<div>login</div>
+		<div className="bglogin">
+			<div className="rowlogin">
+				<div className="mutlogo"></div>
+				<div className="login-form">
+				<h1 className="welcome-login">Welcome Back</h1>
+					<form onSubmit={handleSubmit}>
+						<label className="text_info-login">Username</label>
+						<input
+							type="text"
+							value={inputUsername}
+							onChange={(e) => setInputUsername(e.target.value)}
+							className="info-login"
+							placeholder="Enter your username"
+						/>
+						<label className="text_info-login">Password</label>
+						<input
+							type="password"
+							value={inputPassword}
+							onChange={(e) => setInputPassword(e.target.value)}
+							className="info-login"
+							placeholder="Enter your password"
+						/>
+						<a className="forgot-login" onClick={() => forgotpassword()}>
+							I forgot my password
+						</a>
+						<button className="signin-login" type="submit" name="signin">
+							Sign In
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
 	);
 }
 
