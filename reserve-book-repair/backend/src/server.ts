237538d86ecaff31228845cbcf7a6a96132aca41
Tisarji <jirasitkarunwong@gmail.com>
2025-05-08 17:23:38 +0700

    init workflow

--- reserve-book-repair/backend/src/server.ts
+++ reserve-book-repair/backend/src/server.ts
@@ -1,7 +1,8 @@
 import app from './app'
+import chalk from 'chalk'
 
 const PORT = process.env.PORT || 8080
 
 app.listen(PORT, () => {
-	console.log(`Server is running at http://localhost:${PORT}`)
+	console.log(chalk.green.bold(`💣 Server is running at `) + chalk.bgGray.underline(`http://localhost:${PORT}`));
 })
